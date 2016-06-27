651aced377d40478a00c5dc8bb2bf2825961ae54
devjanaprime <dev@primeacademy.io>
2016-06-27 11:09:25 -0500

    initial angular seems to be working

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,1 +1,8 @@
-console.log( 'scripts.js in da hizzy' );
+var myApp = angular.module( 'myApp', [] );
+
+myApp.controller( 'mondayController',[ '$scope', '$http', function( $scope, $http ){
+  // test get user input
+  $scope.whatAreWeTryingToDo = function(){
+    console.log( 'in whatAreWeTryingToDo: ' + $scope.notATextArea );
+  };
+}]);
